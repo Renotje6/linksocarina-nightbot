bd982dcc2412336f6cf2b3484d087bac3910605f
Renotje6 <38280916+Renotje6@users.noreply.github.com>
2020-08-14 23:13:52 +0200

    Update flowers.js
    - Minified the JavaScript code and made it into a Nightbot command
    - Added a few comments

--- flowers.js
+++ flowers.js
@@ -1,3 +1,13 @@
+// !flowers command
+// Version: 1.0.0
+// ================
+
+// Copy the line BELOW this line for the Nightbot command
+$eval(let q="$(querystring)",let rs="",fls=[{fl:"red",fr:"double next flower"},{fl:"blue",fr:"suits"},{fl:"yellow",fr:"marbles"},{fl:"purple",fr:"100 likes double"},{fl:"orange",fr:"sip"},{fl:"mixed",fr:"choose (no black/white/red)"},{fl:"assorted",fr:"down against the clock"},{fl:"black",fr:"5 shots"},{fl:"white",fr:"quarter bottle"}],adjustedQ=q.toLowerCase(),s=fls.find(f=>f.fl==adjustedQ);s?rs=`[${s.fl}] => ${s.fr}`:fls.forEach(f=>{rs+=`[${f.fl}] => ${f.fr} \n`});rs)
+
+
+
+// JavaScript code
 let rs = "";
 let fls = [
   {
@@ -47,4 +57,4 @@ if (s) {
     rs += `[${fl.fl}] => ${fl.fr} \n`;
   });
 }
-rs
+console.log(rs);
